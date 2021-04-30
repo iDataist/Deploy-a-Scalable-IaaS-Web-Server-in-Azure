c0f52d45a3d74bcf99c3bd62ac417bf014d5057d
Hui Ren <huiren@Huis-MacBook-Pro.local>
2021-04-30 08:50:39 -0500

    [UPDATE]: run 'terraform apply' and updated the files

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,27 +17,27 @@ resource "azurerm_subnet" "internal" {
 }
 
 resource "azurerm_public_ip" "main" {
- name                         = "publicIPForLB"
- location                     = var.location
- resource_group_name          = var.resource_group
- allocation_method            = "Static"
+  name                = "publicIPForLB"
+  location            = var.location
+  resource_group_name = var.resource_group
+  allocation_method   = "Static"
 }
 
 resource "azurerm_lb" "main" {
- name                = "loadBalancer"
- location            = var.location
- resource_group_name = var.resource_group
+  name                = "loadBalancer"
+  location            = var.location
+  resource_group_name = var.resource_group
 
- frontend_ip_configuration {
-   name                 = "publicIPAddress"
-   public_ip_address_id = azurerm_public_ip.main.id
- }
+  frontend_ip_configuration {
+    name                 = "publicIPAddress"
+    public_ip_address_id = azurerm_public_ip.main.id
+  }
 }
 
 resource "azurerm_lb_backend_address_pool" "main" {
- resource_group_name = var.resource_group
- loadbalancer_id     = azurerm_lb.main.id
- name                = "BackEndAddressPool"
+  resource_group_name = var.resource_group
+  loadbalancer_id     = azurerm_lb.main.id
+  name                = "BackEndAddressPool"
 }
 
 resource "azurerm_network_interface" "main" {
@@ -54,64 +54,64 @@ resource "azurerm_network_interface" "main" {
 }
 
 resource "azurerm_network_security_group" "main" {
-    name                = "${var.prefix}-nsg"
-    location            = var.location
-    resource_group_name = var.resource_group
-    
-    security_rule {
-        name                       = "internal_inbound"
-        priority                   = 100
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "*"
-        source_address_prefix      = "10.0.2.0/24"
-        destination_address_prefix = "10.0.2.0/24"
-    }
+  name                = "${var.prefix}-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group
 
-    security_rule {
-        name                       = "internal_outbound"
-        priority                   = 101
-        direction                  = "Outbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "*"
-        source_address_prefix      = "10.0.2.0/24"
-        destination_address_prefix = "10.0.2.0/24"
-    }
+  security_rule {
+    name                       = "internal_inbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "10.0.2.0/24"
+    destination_address_prefix = "10.0.2.0/24"
+  }
 
-    security_rule {
-        name                       = "external_inbound"
-        priority                   = 102
-        direction                  = "Inbound"
-        access                     = "Deny"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "*"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
+  security_rule {
+    name                       = "internal_outbound"
+    priority                   = 101
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "10.0.2.0/24"
+    destination_address_prefix = "10.0.2.0/24"
+  }
+
+  security_rule {
+    name                       = "external_inbound"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_managed_disk" "main" {
- count                = var.instance_count
- name                 = "datadisk_existing_${count.index}"
- location             = var.location
- resource_group_name  = var.resource_group
- storage_account_type = "Standard_LRS"
- create_option        = "Empty"
- disk_size_gb         = "1023"
+  count                = var.instance_count
+  name                 = "datadisk_existing_${count.index}"
+  location             = var.location
+  resource_group_name  = var.resource_group
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "1023"
 }
 
 resource "azurerm_availability_set" "avset" {
- name                         = "avset"
- location                     = var.location
- resource_group_name          = var.resource_group
- platform_fault_domain_count  = var.instance_count
- platform_update_domain_count = var.instance_count
- managed                      = true
+  name                         = "avset"
+  location                     = var.location
+  resource_group_name          = var.resource_group
+  platform_fault_domain_count  = var.instance_count
+  platform_update_domain_count = var.instance_count
+  managed                      = true
 }
 
 resource "azurerm_linux_virtual_machine" "main" {
@@ -125,7 +125,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   admin_username                  = "vmadmin"
   admin_password                  = "p@ssword1234"
   disable_password_authentication = false
-  network_interface_ids = [element(azurerm_network_interface.main.*.id, count.index)]
+  network_interface_ids           = [element(azurerm_network_interface.main.*.id, count.index)]
 
   os_disk {
     storage_account_type = "Standard_LRS"
@@ -133,6 +133,6 @@ resource "azurerm_linux_virtual_machine" "main" {
   }
 
   tags = {
-   DevOps = "Deploy a Web Server in Azure"
- }
+    DevOps = "Deploy a Web Server in Azure"
+  }
 }
--- terraform/var.tf
+++ terraform/var.tf
@@ -1,24 +1,24 @@
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
-  default="20210405"
+  default     = "20210405"
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
-  default="westus2"
+  default     = "westus2"
 }
 
 variable "resource_group" {
   description = "The Azure Resource Group in which all resources in this example should be created."
-  default="rg"
+  default     = "rg"
 }
 
 variable "instance_count" {
   description = "The number of resources."
-  default=2
+  default     = 2
 }
 
 variable "image_resource_id" {
   description = "Packer image resource ID."
-  default="/subscriptions/5bb35c36-233e-4b7e-afd9-a2b795899fb9/resourceGroups/rg/providers/Microsoft.Compute/images/Ubuntu18.04"
+  default     = "/subscriptions/5bb35c36-233e-4b7e-afd9-a2b795899fb9/resourceGroups/rg/providers/Microsoft.Compute/images/Ubuntu18.04"
 }
